d89aee908bde583977ab9d38c207fdd3a7508040
MR GOKU <154590485+VIPBOLTE@users.noreply.github.com>
2025-01-03 19:57:09 +0530

    Update __init__.py

--- Strings/__init__.py
+++ Strings/__init__.py
@@ -1,20 +1,38 @@
+
+# All rights reserved
+
 import os
+import sys
 from typing import List
 
 import yaml
 
 languages = {}
+commands = {}
+
 languages_present = {}
+
+
+def get_command(value: str) -> List:
+    return commands["command"][value]
 
 
 def get_string(lang: str):
     return languages[lang]
 
 
-for filename in os.listdir(r"./Strings/languages/"):
+for filename in os.listdir(r"./strings"):
+    if filename.endswith(".yml"):
+        language_name = filename[:-4]
+        commands[language_name] = yaml.safe_load(
+            open(r"./strings/" + filename, encoding="utf8")
+        )
+
+
+for filename in os.listdir(r"./strings/langs/"):
     if "en" not in languages:
         languages["en"] = yaml.safe_load(
-            open(r"./Strings/languages/en.yml", encoding="utf8")
+            open(r"./strings/langs/en.yml", encoding="utf8")
         )
         languages_present["en"] = languages["en"]["name"]
     if filename.endswith(".yml"):
@@ -22,13 +40,15 @@ for filename in os.listdir(r"./Strings/languages/"):
         if language_name == "en":
             continue
         languages[language_name] = yaml.safe_load(
-            open(r"./Strings/languages/" + filename, encoding="utf8")
+            open(r"./strings/langs/" + filename, encoding="utf8")
         )
         for item in languages["en"]:
             if item not in languages[language_name]:
                 languages[language_name][item] = languages["en"][item]
     try:
         languages_present[language_name] = languages[language_name]["name"]
-    except:
-        print("There is some issue with the language file inside bot.")
-        exit()
+    except Exception as e:
+                import traceback 
+                print(f"Error loading {filename}: {e}")
+                traceback.print_exc()
+        
