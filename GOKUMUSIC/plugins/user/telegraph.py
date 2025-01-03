87e8158c0e7b5d7a608e1c19814ff63ea09008c3
MR GOKU <154590485+VIPBOLTE@users.noreply.github.com>
2025-01-03 22:53:14 +0530

    Create telegraph.py

---
+++ GOKUMUSIC/plugins/user/telegraph.py
@@ -0,0 +1,90 @@
+import os
+from pyrogram import filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from GOKUMUSIC import app
+from GOKUMUSIC.misc import SUDOERS as SUDO_USER
+from TheApi import api
+from pyrogram import filters, Client
+
+@Client.on_message(filters.command("tm", prefixes=".") & SUDO_USER)
+async def get_link_group(client, message):
+    if not message.reply_to_message:
+        return await message.reply_text(
+            "Pʟᴇᴀsᴇ ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴍᴇᴅɪᴀ ᴛᴏ ᴜᴘʟᴏᴀᴅ ᴏɴ Tᴇʟᴇɢʀᴀᴘʜ"
+        )
+
+    media = message.reply_to_message
+    file_size = 0
+    if media.photo:
+        file_size = media.photo.file_size
+    elif media.video:
+        file_size = media.video.file_size
+    elif media.document:
+        file_size = media.document.file_size
+
+    if file_size > 15 * 1024 * 1024:
+        return await message.reply_text("Pʟᴇᴀsᴇ ᴘʀᴏᴠɪᴅᴇ ᴀ ᴍᴇᴅɪᴀ ғɪʟᴇ ᴜɴᴅᴇʀ 15MB.")
+
+    try:
+        text = await message.reply("Pʀᴏᴄᴇssɪɴɢ...")
+
+        async def progress(current, total):
+            try:
+                await text.edit_text(f"📥 Dᴏᴡɴʟᴏᴀᴅɪɴɢ... {current * 100 / total:.1f}%")
+            except Exception:
+                pass
+
+        try:
+            local_path = await media.download(progress=progress)
+            await text.edit_text("📤 Uᴘʟᴏᴀᴅɪɴɢ ᴛᴏ ᴛᴇʟᴇɢʀᴀᴘʜ...")
+
+            upload_path = api.upload_image(local_path)
+
+            await text.edit_text(
+                f"🌐 | [ᴜᴘʟᴏᴀᴅᴇᴅ ʟɪɴᴋ]({upload_path})",
+                reply_markup=InlineKeyboardMarkup(
+                    [
+                        [
+                            InlineKeyboardButton(
+                                "ᴜᴘʟᴏᴀᴅᴇᴅ ғɪʟᴇ",
+                                url=upload_path,
+                            )
+                        ]
+                    ]
+                ),
+            )
+
+            try:
+                os.remove(local_path)
+            except Exception:
+                pass
+
+        except Exception as e:
+            await text.edit_text(f"❌ Fɪʟᴇ ᴜᴘʟᴏᴀᴅ ғᴀɪʟᴇᴅ\n\n<i>Rᴇᴀsᴏɴ: {e}</i>")
+            try:
+                os.remove(local_path)
+            except Exception:
+                pass
+            return
+    except Exception:
+        pass
+
+
+__HELP__ = """
+**ᴛᴇʟᴇɢʀᴀᴘʜ ᴜᴘʟᴏᴀᴅ ʙᴏᴛ ᴄᴏᴍᴍᴀɴᴅs**
+
+ᴜsᴇ ᴛʜᴇsᴇ ᴄᴏᴍᴍᴀɴᴅs ᴛᴏ ᴜᴘʟᴏᴀᴅ ᴍᴇᴅɪᴀ ᴛᴏ ᴛᴇʟᴇɢʀᴀᴘʜ:
+
+- `/tgm`: ᴜᴘʟᴏᴀᴅ ʀᴇᴘʟɪᴇᴅ ᴍᴇᴅɪᴀ ᴛᴏ ᴛᴇʟᴇɢʀᴀᴘʜ.
+- `/tgt`: sᴀᴍᴇ ᴀs `/tgm`.
+- `/telegraph`: sᴀᴍᴇ ᴀs `/tgm`.
+- `/tl`: sᴀᴍᴇ ᴀs `/tgm`.
+
+**ᴇxᴀᴍᴘʟᴇ:**
+- ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴘʜᴏᴛᴏ ᴏʀ ᴠɪᴅᴇᴏ ᴡɪᴛʜ `/tgm` ᴛᴏ ᴜᴘʟᴏᴀᴅ ɪᴛ.
+
+**ɴᴏᴛᴇ:**
+ʏᴏᴜ ᴍᴜsᴛ ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴍᴇᴅɪᴀ ғɪʟᴇ ғᴏʀ ᴛʜᴇ ᴜᴘʟᴏᴀᴅ ᴛᴏ ᴡᴏʀᴋ.
+"""
+
+__MODULE__ = "Tᴇʟᴇɢʀᴀᴘʜ"
