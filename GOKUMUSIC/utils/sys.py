dfa3e02ca67d835f714b6652d1636068a623751b
MR GOKU <154590485+VIPBOLTE@users.noreply.github.com>
2025-01-03 15:01:50 +0530

    Update sys.py

--- GOKUMUSIC/utils/sys.py
+++ GOKUMUSIC/utils/sys.py
@@ -1,15 +1,22 @@
+# Copyright (C) 2024 by Badhacker98@Github, < https://github.com/Badhacker98 >.
+# Owner https://t.me/ll_BAD_MUNDA_ll
+
 import time
 
 import psutil
 
-from GOKUMUSIC.music import _boot_
-from GOKUMUSIC.utils.formatters import get_readable_time
+from GOKUMUSIC.misc import _boot_
+
+from .formatters import get_readable_time
 
 
 async def bot_sys_stats():
     bot_uptime = int(time.time() - _boot_)
-    UP = f"{get_readable_time(bot_uptime)}"
-    CPU = f"{psutil.cpu_percent(interval=0.5)}%"
-    RAM = f"{psutil.virtual_memory().percent}%"
-    DISK = f"{psutil.disk_usage('/').percent}%"
+    cpu = psutil.cpu_percent(interval=0.5)
+    mem = psutil.virtual_memory().percent
+    disk = psutil.disk_usage("/").percent
+    UP = f"{get_readable_time((bot_uptime))}"
+    CPU = f"{cpu}%"
+    RAM = f"{mem}%"
+    DISK = f"{disk}%"
     return UP, CPU, RAM, DISK
