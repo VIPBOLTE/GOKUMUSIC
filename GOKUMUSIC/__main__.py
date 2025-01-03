88704802e5c7521d3d3beb5b087cc5df1dadef4e
MR GOKU <154590485+VIPBOLTE@users.noreply.github.com>
2025-01-03 14:55:00 +0530

    Update __main__.py

--- GOKUMUSIC/__main__.py
+++ GOKUMUSIC/__main__.py
@@ -2,15 +2,13 @@ import asyncio
 import importlib
 
 from pyrogram import idle
-from pytgcalls.exceptions import NoActiveGroupCall
 
 import config
-from GOKUMUSIC import LOGGER, app, userbot
-from GOKUMUSIC.core.call import GOKU
-from GOKUMUSIC.music import sudo
+from config import BANNED_USERS
+from GOKUMUSIC import HELPABLE, LOGGER, app, userbot
+from GOKUMUSIC.core.call import BAD
 from GOKUMUSIC.plugins import ALL_MODULES
 from GOKUMUSIC.utils.database import get_banned_users, get_gbanned
-from config import BANNED_USERS
 
 
 async def init():
@@ -21,9 +19,15 @@ async def init():
         and not config.STRING4
         and not config.STRING5
     ):
-        LOGGER(__name__).error("𝐒𝐭𝐫𝐢𝐧𝐠 𝐒𝐞𝐬𝐬𝐢𝐨𝐧 𝐍𝐨𝐭 𝐅𝐢𝐥𝐥𝐞𝐝, 𝐏𝐥𝐞𝐚𝐬𝐞 𝐅𝐢𝐥𝐥 𝐀 𝐏𝐲𝐫𝐨𝐠𝐫𝐚𝐦 𝐒𝐞𝐬𝐬𝐢𝐨𝐧")
-        exit()
-    await sudo()
+        LOGGER("GOKUMUSIC").error(
+            "No Assistant Clients Vars Defined!.. Exiting Process."
+        )
+        return
+    if not config.SPOTIFY_CLIENT_ID and not config.SPOTIFY_CLIENT_SECRET:
+        LOGGER("GOKUMUSIC").warning(
+            "No Spotify Vars defined. Your bot won't be able to play spotify queries."
+        )
+
     try:
         users = await get_gbanned()
         for user_id in users:
@@ -31,32 +35,26 @@ async def init():
         users = await get_banned_users()
         for user_id in users:
             BANNED_USERS.add(user_id)
-    except:
+    except Exception:
         pass
+
     await app.start()
+
     for all_module in ALL_MODULES:
-        importlib.import_module("GOKUMUSIC.plugins" + all_module)
-    LOGGER("GOKUMUSIC.plugins").info("𝐀𝐥𝐥 𝐅𝐞𝐚𝐭𝐮𝐫𝐞𝐬 𝐋𝐨𝐚𝐝𝐞𝐝 𝐁𝐚𝐛𝐲🥳...")
+        imported_module = importlib.import_module(all_module)
+
+        if hasattr(imported_module, "__MODULE__") and imported_module.__MODULE__:
+            if hasattr(imported_module, "__HELP__") and imported_module.__HELP__:
+                HELPABLE[imported_module.__MODULE__.lower()] = imported_module
+    LOGGER("GOKUMUSIC.plugins").info("Successfully Imported All Modules ")
+
     await userbot.start()
-    await GOKU.start()
-    try:
-        await GOKU.stream_call("https://telegra.ph/file/289cb2d7166b7e2d6c1a8.mp4")
-    except NoActiveGroupCall:
-        LOGGER("GOKUMUSIC").error(
-            "𝗣𝗹𝗭 𝗦𝗧𝗔𝗥𝗧 𝗬𝗢𝗨𝗥 𝗟𝗢𝗚 𝗚𝗥𝗢𝗨𝗣 𝗩𝗢𝗜𝗖𝗘𝗖𝗛𝗔𝗧\𝗖𝗛𝗔𝗡𝗡𝗘𝗟\n\n𝗚𝗢𝗞𝗨 𝗕𝗢𝗧 𝗦𝗧𝗢𝗣........"
-        )
-        exit()
-    except:
-        pass
-    await GOKU.decorators()
-    LOGGER("GOKUMUSIC").info(
-        "╔═════ஜ۩۞۩ஜ════╗\n  ☠︎︎𝗠𝗔𝗗𝗘 𝗕𝗬 𝗠𝗥 𝗚𝗢𝗞𝗨☠︎︎\n╚═════ஜ۩۞۩ஜ════╝"
-    )
+    await BAD.start()
+    await BAD.decorators()
+    LOGGER("GOKUMUSIC").info("GOKUMUSIC STARTED SUCCESSFULLY 🕊️")
     await idle()
-    await app.stop()
-    await userbot.stop()
-    LOGGER("GOKUMUSIC").info("𝗦𝗧𝗢𝗣 𝗚𝗢𝗞𝗨 𝗠𝗨𝗦𝗜𝗖🎻 𝗕𝗢𝗧..")
 
 
 if __name__ == "__main__":
-    asyncio.get_event_loop().run_until_complete(init())
+    asyncio.get_event_loop_policy().get_event_loop().run_until_complete(init())
+    LOGGER("GOKUMUSIC").info("Stopping GOKUMUSIC! GoodBye")
