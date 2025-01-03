d57f39e357b846504875ac98d13672e6c2e99f56
MR GOKU <154590485+VIPBOLTE@users.noreply.github.com>
2025-01-03 16:18:36 +0530

    Update song.py

--- GOKUMUSIC/utils/inline/song.py
+++ GOKUMUSIC/utils/inline/song.py
@@ -1,7 +1,4 @@
-
 from pyrogram.types import InlineKeyboardButton
-
-
 def song_markup(_, vidid):
     buttons = [
         [
@@ -15,9 +12,7 @@ def song_markup(_, vidid):
             ),
         ],
         [
-            InlineKeyboardButton(
-                text=_["CLOSE_BUTTON"], callback_data="close"
-            ),
+            InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close"),
         ],
     ]
     return buttons
