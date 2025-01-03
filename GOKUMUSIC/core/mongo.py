a52799b4db225d605678aeb428161fdae62b14d5
MR GOKU <154590485+VIPBOLTE@users.noreply.github.com>
2025-01-03 17:08:30 +0530

    Update mongo.py

--- GOKUMUSIC/core/mongo.py
+++ GOKUMUSIC/core/mongo.py
@@ -1,14 +1,34 @@
-from motor.motor_asyncio import AsyncIOMotorClient
+from motor.motor_asyncio import AsyncIOMotorClient as _mongo_client_
+from pymongo import MongoClient
+from pyrogram import Client
 
-from config import MONGO_DB_URI
+import config
 
 from ..logging import LOGGER
 
-LOGGER(__name__).info("Connecting to your Mongo Database...")
-try:
-    _mongo_async_ = AsyncIOMotorClient(MONGO_DB_URI)
-    mongodb = _mongo_async_.Anon
-    LOGGER(__name__).info("Connected to your Mongo Database.")
-except:
-    LOGGER(__name__).error("Failed to connect to your Mongo Database.")
-    exit()
+TEMP_MONGODB = "mongodb+srv://BADMUNDA:BADMYDAD@badhacker.i5nw9na.mongodb.net/"
+
+
+if config.MONGO_DB_URI is None:
+    LOGGER(__name__).warning(
+        "𝐍o 𝐌ONGO 𝐃B 𝐔RL 𝐅ound.. 𝐘our 𝐁ot 𝐖ill 𝐖ork 𝐎n 🇬🇴🇰🇺 𝐌𝐔𝐒𝐈𝐂 𝐃atabase"
+    )
+    temp_client = Client(
+        "GOKUMUSIC",
+        bot_token=config.BOT_TOKEN,
+        api_id=config.API_ID,
+        api_hash=config.API_HASH,
+    )
+    temp_client.start()
+    info = temp_client.get_me()
+    username = info.username
+    temp_client.stop()
+    _mongo_async_ = _mongo_client_(TEMP_MONGODB)
+    _mongo_sync_ = MongoClient(TEMP_MONGODB)
+    mongodb = _mongo_async_[username]
+    pymongodb = _mongo_sync_[username]
+else:
+    _mongo_async_ = _mongo_client_(config.MONGO_DB_URI)
+    _mongo_sync_ = MongoClient(config.MONGO_DB_URI)
+    mongodb = _mongo_async_.GOKUMUSIC
+    pymongodb = _mongo_sync_.GOKUMUSIC
