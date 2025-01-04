56eeb534308d422bb267885bbf2d54b9459aa313
MR GOKU <154590485+VIPBOLTE@users.noreply.github.com>
2025-01-04 18:43:50 +0530

    Create ~reaction.py

---
+++ GOKUMUSIC/cplugin/~reaction.py
@@ -0,0 +1,13 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message
+import random
+
+EMOJIS = ["🥰", "❤️", "😁", "💋", "😱", "🤣", "😘", "❤️‍🔥", "👌", "🫡", "😍"]
+
+@Client.on_message(filters.incoming)
+async def react_to_messages(client: Client, message: Message):
+    try:
+        random_emoji = random.choice(EMOJIS)
+        await message.react(random_emoji)
+    except Exception as e:
+        print(f"Failed to react to message: {e}")
