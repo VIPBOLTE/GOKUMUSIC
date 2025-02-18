25de193f96c568e1657a1a63ae83f357f07a6ed7
MR GOKU <154590485+VIPBOLTE@users.noreply.github.com>
2025-02-18 20:08:18 +0530

    Add files via upload

---
+++ Platforms/__init__.py
@@ -0,0 +1,13 @@
+from .Apple import AppleAPI
+
+from .Carbon import CarbonAPI
+
+from .Resso import RessoAPI
+
+from .SoundCloud import SoundAPI
+
+from .Spotify import SpotifyAPI
+
+from .Telegram import TeleAPI
+
+from .YouTube import YouTubeAPI
