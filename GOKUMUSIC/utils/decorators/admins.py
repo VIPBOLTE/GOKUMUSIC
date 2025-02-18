907a7649473ca67117eec285d0bfa007ca5e7021
MR GOKU <154590485+VIPBOLTE@users.noreply.github.com>
2025-02-18 23:32:02 +0530

    Update admins.py

--- GOKUMUSIC/utils/decorators/admins.py
+++ GOKUMUSIC/utils/decorators/admins.py
@@ -14,7 +14,7 @@ from GOKUMUSIC.utils.database import (
     is_skipmode,
 )
 from config import SUPPORT_CHAT, adminlist, confirmer
-from Strings import get_string
+from strings import get_string
 
 from ..formatters import int_to_alpha
 
