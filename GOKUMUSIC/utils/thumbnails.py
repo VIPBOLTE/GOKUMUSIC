dd83a21a1e8aff40252eeb9e64e72e815c09e470
MR GOKU <154590485+VIPBOLTE@users.noreply.github.com>
2025-02-19 21:49:34 +0530

    Update thumbnails.py

--- GOKUMUSIC/utils/thumbnails.py
+++ GOKUMUSIC/utils/thumbnails.py
@@ -172,3 +172,13 @@ async def gen_qthumb(vidid):
     except Exception as e:
         return YOUTUBE_IMG_URL
 
+async def gen_thumb(videoid):
+    try:
+        query = f"https://www.youtube.com/watch?v={videoid}"
+        results = VideosSearch(query, limit=1)
+        for result in (await results.next())["result"]:
+            thumbnail = result["thumbnails"][0]["url"].split("?")[0]
+        return thumbnail
+    except Exception as e:
+        return YOUTUBE_IMG_URL
+        
