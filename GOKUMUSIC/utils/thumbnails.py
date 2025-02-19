ab4a22c0aab7aca19c3b984809f23f8b6e423d88
MR GOKU <154590485+VIPBOLTE@users.noreply.github.com>
2025-02-19 22:11:40 +0530

    Update thumbnails.py

--- GOKUMUSIC/utils/thumbnails.py
+++ GOKUMUSIC/utils/thumbnails.py
@@ -65,8 +65,6 @@ def crop_center_circle(img, output_size, border, crop_scale=1.5):
     return result
 
 
-
-async def get_thumb(videoid):
 async def get_thumb(videoid):
     if os.path.isfile(f"cache/{videoid}_v4.png"):
         return f"cache/{videoid}_v4.png"
