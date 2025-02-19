9d1c87e0bae1824a779e8f51de360e1e5ce80ae0
MR GOKU <154590485+VIPBOLTE@users.noreply.github.com>
2025-02-19 11:56:35 +0530

    Update start.py

--- GOKUMUSIC/plugins/bot/start.py
+++ GOKUMUSIC/plugins/bot/start.py
@@ -246,7 +246,7 @@ async def start_comm(client, message: Message, _):
 
         except AttributeError:
             chat_photo = "assets/nodp.jpg"
-        await message.reply_video(
+        await message.reply_photo(
             photo=chat_photo,
             caption=_["start_2"].format(message.from_user.mention, app.mention),
             reply_markup=InlineKeyboardMarkup(out),
