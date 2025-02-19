dca0fb56ba2fb8ecc40f56db2d94e772687c3473
MR GOKU <154590485+VIPBOLTE@users.noreply.github.com>
2025-02-19 13:06:00 +0530

    Update database.py

--- GOKUMUSIC/utils/database.py
+++ GOKUMUSIC/utils/database.py
@@ -86,7 +86,7 @@ async def set_assistant(chat_id):
 
 
 async def get_assistant(chat_id: int) -> str:
-    from DAXXMUSIC.core.userbot import assistants
+    from GOKUMUSIC.core.userbot import assistants
 
     assistant = assistantdict.get(chat_id)
     if not assistant:
@@ -113,7 +113,7 @@ async def get_assistant(chat_id: int) -> str:
 
 
 async def set_calls_assistant(chat_id):
-    from DAXXMUSIC.core.userbot import assistants
+    from GOKUMUSIC.core.userbot import assistants
 
     ran_assistant = random.choice(assistants)
     assistantdict[chat_id] = ran_assistant
@@ -126,7 +126,7 @@ async def set_calls_assistant(chat_id):
 
 
 async def group_assistant(self, chat_id: int) -> int:
-    from DAXXMUSIC.core.userbot import assistants
+    from GOKUMUSIC.core.userbot import assistants
 
     assistant = assistantdict.get(chat_id)
     if not assistant:
