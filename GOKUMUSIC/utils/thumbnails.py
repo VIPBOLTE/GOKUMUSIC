98a0dd4d7efd14e4ccf3dce7d65d64801607c69e
MR GOKU <154590485+VIPBOLTE@users.noreply.github.com>
2025-02-22 02:48:44 +0530

    Update thumbnails.py

--- GOKUMUSIC/utils/thumbnails.py
+++ GOKUMUSIC/utils/thumbnails.py
@@ -110,8 +110,9 @@ async def get_thumb(videoid):
     draw.text((right_x, 400), duration_text, (255, 255, 255), font=font)
 
     # **Red Line Drawing with increased width for bolder appearance**
+    # **Red Line Drawing with increased width for bolder appearance**
     line_start_x = blurred_background.width / 2
-    line_start_y = blurred_background.height / 2 + 50  # Move it further down from the center
+    line_start_y = blurred_background.height / 2 + 30  # Move it further up from the center
     line_end_x = blurred_background.width - 50  # End at the right side
     line_end_y = line_start_y  # Keep the line horizontal
 
