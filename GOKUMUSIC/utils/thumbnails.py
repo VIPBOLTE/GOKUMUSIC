da53c3591dd63c0d7cecec7a1e5dd7710ff48027
MR GOKU <154590485+VIPBOLTE@users.noreply.github.com>
2025-02-21 14:01:08 +0530

    Update thumbnails.py

--- GOKUMUSIC/utils/thumbnails.py
+++ GOKUMUSIC/utils/thumbnails.py
@@ -11,7 +11,7 @@ from PIL import ImageFilter, ImageFont, ImageOps
 from unidecode import unidecode
 from youtubesearchpython.__future__ import VideosSearch
 
-from BrandrdXMusic import app
+from GOKUMUSIC import app
 from config import YOUTUBE_IMG_URL
 
 def changeImageSize(maxWidth, maxHeight, image):
