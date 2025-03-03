53d491f288017f8b44553c6f9505dd95daa9a698
MR GOKU <154590485+VIPBOLTE@users.noreply.github.com>
2025-03-03 15:07:51 +0530

    Update couples.py

--- GOKUMUSIC/plugins/modules/couples.py
+++ GOKUMUSIC/plugins/modules/couples.py
@@ -1,110 +1,167 @@
-import os
+import os 
 import random
-from datetime import datetime
+from datetime import datetime 
 from telegraph import upload_file
-from PIL import Image, ImageDraw
-from pyrogram import Client, filters
+from PIL import Image , ImageDraw
+from pyrogram import *
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-from pyrogram.enums import ChatType
+from pyrogram.enums import *
 
-# Import the bot instance
-from GOKUMUSIC import app as app  
+#BOT FILE NAME
+from GOKUMUSIC import app as app
+from GOKUMUSIC.mongo.couples_db import _get_image, get_couple
 
-# Define Keyboard
 POLICE = [
     [
         InlineKeyboardButton(
             text="𓊈𝗚𝗢𝗞𝗨 𝗕𝗢𝗧 𝗠𝗔𝗞𝗘𝗥𓊉",
-            url="https://t.me/goku_groupz",
+            url=f"https://t.me/goku_groupz",
         ),
     ],
 ]
 
-# Date Functions
+
 def dt():
     now = datetime.now()
-    return now.strftime("%d/%m/%Y"), now.strftime("%H:%M")
+    dt_string = now.strftime("%d/%m/%Y %H:%M")
+    dt_list = dt_string.split(" ")
+    return dt_list
+    
 
 def dt_tom():
-    today, _ = dt()
-    day, month, year = map(int, today.split("/"))
-    return f"{day+1}/{month}/{year}"
+    a = (
+        str(int(dt()[0].split("/")[0]) + 1)
+        + "/"
+        + dt()[0].split("/")[1]
+        + "/"
+        + dt()[0].split("/")[2]
+    )
+    return a
 
-tomorrow = dt_tom()
-today = dt()[0]
+tomorrow = str(dt_tom())
+today = str(dt()[0])
 
 @app.on_message(filters.command("couples"))
 async def ctest(_, message):
     cid = message.chat.id
-
     if message.chat.type == ChatType.PRIVATE:
-        return await message.reply_text("This command only works in groups.")
-
-    msg = await message.reply_text("Generating couples image...")
-
-    # Get list of users (excluding bots)
-    list_of_users = [
-        i.user.id async for i in app.get_chat_members(message.chat.id, limit=50) if not i.user.is_bot
-    ]
-
-    if len(list_of_users) < 2:
-        return await msg.edit("Not enough users in the group to form a couple.")
-
-    # Randomly select two unique users
-    c1_id, c2_id = random.sample(list_of_users, 2)
-
-    # Get profile pictures
+        return await message.reply_text("ᴛʜɪs ᴄᴏᴍᴍᴀɴᴅ ᴏɴʟʏ ᴡᴏʀᴋs ɪɴ ɢʀᴏᴜᴘs.")
     try:
-        p1 = await app.download_media((await app.get_chat(c1_id)).photo.big_file_id, file_name=f"pfp1_{cid}.png")
-    except:
-        p1 = "assets/default.png"  # Fallback image
+     #  is_selected = await get_couple(cid, today)
+     #  if not is_selected:
+         msg = await message.reply_text("ɢᴇɴᴇʀᴀᴛɪɴɢ ᴄᴏᴜᴘʟᴇs ɪᴍᴀɢᴇ...")
+         #GET LIST OF USERS
+         list_of_users = []
 
-    try:
-        p2 = await app.download_media((await app.get_chat(c2_id)).photo.big_file_id, file_name=f"pfp2_{cid}.png")
-    except:
-        p2 = "assets/default.png"
+         async for i in app.get_chat_members(message.chat.id, limit=50):
+             if not i.user.is_bot:
+               list_of_users.append(i.user.id)
 
-    # Open images and process
-    img1, img2 = Image.open(p1).resize((437, 437)), Image.open(p2).resize((437, 437))
+         c1_id = random.choice(list_of_users)
+         c2_id = random.choice(list_of_users)
+         while c1_id == c2_id:
+              c1_id = random.choice(list_of_users)
+
+
+         photo1 = (await app.get_chat(c1_id)).photo
+         photo2 = (await app.get_chat(c2_id)).photo
+ 
+         N1 = (await app.get_users(c1_id)).mention 
+         N2 = (await app.get_users(c2_id)).mention
+         
+         try:
+            p1 = await app.download_media(photo1.big_file_id, file_name="pfp.png")
+         except Exception:
+            p1 = "GOKUMUSIC/assets/upic.png"
+         try:
+            p2 = await app.download_media(photo2.big_file_id, file_name="pfp1.png")
+         except Exception:
+            p2 = "GOKUMUSIC/assets/upic.png"
+            
+         img1 = Image.open(f"{p1}")
+         img2 = Image.open(f"{p2}")
+
+         img = Image.open("GOKUMUSIC/assets/cppic.png")
+
+         img1 = img1.resize((437,437))
+         img2 = img2.resize((437,437))
+
+         mask = Image.new('L', img1.size, 0)
+         draw = ImageDraw.Draw(mask) 
+         draw.ellipse((0, 0) + img1.size, fill=255)
+
+         mask1 = Image.new('L', img2.size, 0)
+         draw = ImageDraw.Draw(mask1) 
+         draw.ellipse((0, 0) + img2.size, fill=255)
+
+
+         img1.putalpha(mask)
+         img2.putalpha(mask1)
+
+         draw = ImageDraw.Draw(img)
+
+         img.paste(img1, (116, 160), img1)
+         img.paste(img2, (789, 160), img2)
+
+         img.save(f'test_{cid}.png')
     
-    # Create circular masks
-    mask = Image.new('L', img1.size, 0)
-    draw = ImageDraw.Draw(mask)
-    draw.ellipse((0, 0) + img1.size, fill=255)
+         TXT = f"""
+**ᴛᴏᴅᴀʏ's ᴄᴏᴜᴘʟᴇ ᴏғ ᴛʜᴇ ᴅᴀʏ :
 
-    img1.putalpha(mask)
-    img2.putalpha(mask)
+{N1} + {N2} = ❤💖💝
 
-    # Merge into template
-    img = Image.open("assets/cppic.png")
-    img.paste(img1, (116, 160), img1)
-    img.paste(img2, (789, 160), img2)
-    img.save(f"couple_{cid}.png")
-
-    # Get user mentions
-    N1, N2 = (await app.get_users(c1_id)).mention, (await app.get_users(c2_id)).mention
-
-    # Send result
-    caption = f"**Today's Couple of the Day:**\n\n{N1} + {N2} = ❤️💖\n\nNext couples will be selected on {tomorrow}!"
-    await message.reply_photo(f"couple_{cid}.png", caption=caption, reply_markup=InlineKeyboardMarkup(POLICE))
-    await msg.delete()
-
-    # Upload to Telegraph
-    try:
-        uploaded_files = upload_file(f"couple_{cid}.png")
-        telegraph_url = "https://graph.org/" + uploaded_files[0]
-        print(f"Uploaded Image URL: {telegraph_url}")
+ɴᴇxᴛ ᴄᴏᴜᴘʟᴇs ᴡɪʟʟ ʙᴇ sᴇʟᴇᴄᴛᴇᴅ ᴏɴ {tomorrow} !!**
+"""
+    
+         await message.reply_photo(f"test_{cid}.png", caption=TXT, reply_markup=InlineKeyboardMarkup(POLICE),
+    )
+         await msg.delete()
+         a = upload_file(f"test_{cid}.png")
+         for x in a:
+           img = "https://graph.org/" + x
+           couple = {"c1_id": c1_id, "c2_id": c2_id}
+          # await save_couple(cid, today, couple, img)
+    
+         
+      # elif is_selected:
+      #   msg = await message.reply_text("𝐆ᴇᴛᴛɪɴɢ 𝐓ᴏᴅᴀʏs 𝐂ᴏᴜᴘʟᴇs 𝐈ᴍᴀɢᴇ...")
+      #   b = await _get_image(cid)
+       #  c1_id = int(is_selected["c1_id"])
+       #  c2_id = int(is_selected["c2_id"])
+       #  c1_name = (await app.get_users(c1_id)).first_name
+        # c2_name = (await app.get_users(c2_id)).first_name
+         
+      #   TXT = f"""
+#**𝐓ᴏᴅᴀʏ's 𝐒ᴇʟᴇᴄᴛᴇᴅ 𝐂ᴏᴜᴘʟᴇs 🎉 :
+#➖➖➖➖➖➖➖➖➖➖➖➖
+#[{c1_name}](tg://openmessage?user_id={c1_id}) + [{c2_name}](tg://openmessage?user_id={c2_id}) = ❣️
+#➖➖➖➖➖➖➖➖➖➖➖➖
+#𝐍ᴇxᴛ 𝐂ᴏᴜᴘʟᴇs 𝐖ɪʟʟ 𝐁ᴇ 𝐒ᴇʟᴇᴄᴛᴇᴅ 𝐎ɴ {tomorrow} !!**
+#"""
+ #        await message.reply_photo(b, caption=TXT)
+        # await msg.delete()
     except Exception as e:
-        print(f"Error uploading to Telegraph: {e}")
-
-    # Clean up files
-    os.remove(f"couple_{cid}.png")
-    if os.path.exists(p1):
-        os.remove(p1)
-    if os.path.exists(p2):
-        os.remove(p2)
+        print(str(e))
+    try:
+      os.remove(f"./downloads/pfp1.png")
+      os.remove(f"./downloads/pfp2.png")
+      os.remove(f"test_{cid}.png")
+    except Exception:
+       pass
+         
 
 __mod__ = "COUPLES"
 __help__ = """
-**» /couples** - Get today's selected couples in an interactive image view.
+**» /couples** - Get Todays Couples Of The Group In Interactive View
 """
+
+
+
+
+
+    
+
+
+
+
+    
